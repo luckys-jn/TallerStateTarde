3e2ef79b96df7285eeac8f689eb441095e1e282d
luckys-jn <aramirez181@unab.edu.co>
2026-09-24 23:31:00 -0500

    Dados

--- app/src/main/java/me/angelramirez/statesjetpackcompose/PrimerPantalla.kt
+++ app/src/main/java/me/angelramirez/statesjetpackcompose/PrimerPantalla.kt
@@ -1,43 +1,59 @@
 package me.angelramirez.statesjetpackcompose
 
-import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
-@Preview(showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
+
 fun PrimerPantalla() {
 
-    //var contador = 3
-    var contador by remember { mutableStateOf(3) }
+    val diceImages = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
+
+    var diceResult by remember { mutableIntStateOf(1) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Contador:  $contador"   , fontSize = 32.sp)
-        Spacer(modifier = Modifier.height(25.dp))
-        Button(onClick = {
-            Log.i("mis-errores", "Estoy dando click")
-            contador = contador + 1
-        }) {
-            Text("Sumar 1")
+        Image(
+            painter = painterResource(id = diceImages[diceResult - 1]),
+            contentDescription = "Dado cara $diceResult",
+            modifier = Modifier.size(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            onClick = {
+                diceResult = (1..6).random()
+            }
+        ) {
+            Text("Lanza el dado!!!!")
         }
     }
 }
